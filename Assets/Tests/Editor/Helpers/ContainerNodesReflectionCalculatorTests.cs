fa4d6917a2d25d2e848e0bc2bc75512183eef8d4
Pavel Prokopov <pavel.prokopov@murka.com>
2018-05-29 17:03:34 +0300

    [+] Added tests

---
+++ Assets/Tests/Editor/Helpers/ContainerNodesReflectionCalculatorTests.cs
@@ -0,0 +1,54 @@
+﻿
+using Helpers;
+using NUnit.Framework;
+using Assert = UnityEngine.Assertions.Assert;
+
+
+public class ContainerNodesReflectionCalculatorTests
+{
+    [Test]
+    public void Test_CalculateMethod_Contaier()
+    {
+        int itemsCount = 1000000;
+        ICalculator<int, Container> calculator = new ContainerNodesReflectionCalculator<Container>();
+        Container container = new Container(itemsCount);
+        
+        Assert.AreEqual(itemsCount, calculator.Calculate(container));
+    }
+    
+    [Test]
+    public void Test_CalculateMethod_ContaierWrapper()
+    {
+        int itemsCount = 1000000;
+        ICalculator<int, ContainerWrapper> calculator = new ContainerNodesReflectionCalculator<ContainerWrapper>();
+        ContainerWrapper container = new ContainerWrapper(itemsCount);
+        
+        Assert.AreEqual(itemsCount, calculator.Calculate(container));
+    }
+
+    [Test]
+    public void Test_CheckCurrentNodeAfterCalculateMethod_Contaier()
+    {
+        int itemsCount = 1000000;
+        ICalculator<int, Container> calculator = new ContainerNodesReflectionCalculator<Container>();
+        Container container = new Container(itemsCount);
+        var currentNode = container.GetCurrentNode();
+        
+        calculator.Calculate(new Container(itemsCount));
+        var isEquals = ReferenceEquals(currentNode, container.GetCurrentNode());
+        Assert.AreEqual(isEquals, true);
+    }
+    
+    [Test]
+    public void Test_CheckCurrentNodeAfterCalculateMethod_ContainerWrapper()
+    {
+        int itemsCount = 1000000;
+        ICalculator<int, ContainerWrapper> calculator = new ContainerNodesReflectionCalculator<ContainerWrapper>();
+        Container container = new Container(itemsCount);
+        var currentNode = container.GetCurrentNode();
+        
+        calculator.Calculate(new ContainerWrapper(itemsCount));
+        var isEquals = ReferenceEquals(currentNode, container.GetCurrentNode());
+        Assert.AreEqual(isEquals, true);
+    }
+}
