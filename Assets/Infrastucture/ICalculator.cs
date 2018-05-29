80cae20542cbcf431d192434934cd7d279093559
Pavel Prokopov <pavel.prokopov@murka.com>
2018-05-29 16:47:53 +0300

    [+] Added infrastucture

---
+++ Assets/Infrastucture/ICalculator.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface ICalculator<T, U>
+{
+    T Calculate(U u);
+}
---
+++ Assets/Infrastucture/IContainer.cs
@@ -0,0 +1,10 @@
+﻿public interface IContainer
+{
+    int Count { get; }
+    
+    bool Value { get; set; }
+    
+    void MoveForward();
+
+    void MoveBackward();
+}
